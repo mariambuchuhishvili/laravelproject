fffd271f86450bbf61cfaf3d9945cc4b195fc7e7
mariam <mariammuza@mail.ru>
2022-05-25 17:10:14 +0500

    commit_wednesday_two

--- resources/views/my_page.blade.php
+++ resources/views/my_page.blade.php
@@ -23,18 +23,18 @@
     <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
         <div class="col">
             <div class="card shadow-sm">
-                <svg class="bd-placeholder-img card-img-top" width="100%" height="225"  xmlns="http://www.w3.org/2000/svg"  role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false">
-                    <img src="{{ asset('img/logo.png') }}" alt="">
-                    <rect width="100%" height="100%" fill="#eceeef"></rect>
-                    <text x="50%" y="50%" fill="#eceeef" dy=".3em">Букет роз</text>
+                <svg class="bd-placeholder-img card-img-top"   width="477" height="225"  xmlns="http://www.w3.org/2000/svg"  role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false">
+                    <image href="https://newflora.ru/wp-content/uploads/2019/05/kernew-Newflora.ru-1.jpg" class="img-fluid"   width="477" height="225"/> -->
+                    <!--<rect width="100%" height="100%" fill="#eceeef"></rect>-->
+
                 </svg>
 
                 <div class="card-body">
                     <p class="card-text"> Букет из 3 видов роз. </p>
                     <div class="d-flex justify-content-between align-items-center">
                         <div class="btn-group">
-                            <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                            <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                            <button type="button" class="btn btn-sm btn-outline-secondary">Подробно</button>
+                            <button type="button" class="btn btn-sm btn-outline-secondary" style="background-color: violet">В корзину</button>
                         </div>
                         <small class="text-muted">1500 руб.</small>
                     </div>
@@ -43,81 +43,40 @@
         </div>
         <div class="col">
             <div class="card shadow-sm">
-                <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+                <svg class="bd-placeholder-img card-img-top"   width="477" height="225"  xmlns="http://www.w3.org/2000/svg"  role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false">
+                    <image href="https://newflora.ru/wp-content/uploads/2019/05/kernew-Newflora.ru-1.jpg" class="img-fluid"   width="477" height="225"/> -->
+                    <!--<rect width="100%" height="100%" fill="#eceeef"></rect>-->
+
+                </svg>
 
                 <div class="card-body">
-                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <p class="card-text"> Букет из роз. </p>
                     <div class="d-flex justify-content-between align-items-center">
                         <div class="btn-group">
-                            <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                            <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                            <button type="button" class="btn btn-sm btn-outline-secondary">Подробно</button>
+                            <button type="button" class="btn btn-sm btn-outline-secondary" style="background-color: violet">В корзину</button>
                         </div>
-                        <small class="text-muted">9 mins</small>
+                        <small class="text-muted">1500 руб.</small>
                     </div>
                 </div>
             </div>
         </div>
         <div class="col">
             <div class="card shadow-sm">
-                <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+                <svg class="bd-placeholder-img card-img-top"   width="477" height="225"  xmlns="http://www.w3.org/2000/svg"  role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false">
+                    <image href="https://newflora.ru/wp-content/uploads/2019/05/kernew-Newflora.ru-1.jpg" class="img-fluid"   width="477" height="225"/> -->
+                    <!--<rect width="100%" height="100%" fill="#eceeef"></rect>-->
+
+                </svg>
 
                 <div class="card-body">
-                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <p class="card-text"> Букет из 3 видов роз. </p>
                     <div class="d-flex justify-content-between align-items-center">
                         <div class="btn-group">
-                            <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                            <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                            <button type="button" class="btn btn-sm btn-outline-secondary">Подробно</button>
+                            <button type="button" class="btn btn-sm btn-outline-secondary" style="background-color: violet">В корзину</button>
                         </div>
-                        <small class="text-muted">9 mins</small>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <div class="col">
-            <div class="card shadow-sm">
-                <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-
-                <div class="card-body">
-                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                    <div class="d-flex justify-content-between align-items-center">
-                        <div class="btn-group">
-                            <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                            <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                        </div>
-                        <small class="text-muted">9 mins</small>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="col">
-            <div class="card shadow-sm">
-                <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-
-                <div class="card-body">
-                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                    <div class="d-flex justify-content-between align-items-center">
-                        <div class="btn-group">
-                            <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                            <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                        </div>
-                        <small class="text-muted">9 mins</small>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="col">
-            <div class="card shadow-sm">
-                <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-
-                <div class="card-body">
-                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                    <div class="d-flex justify-content-between align-items-center">
-                        <div class="btn-group">
-                            <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                            <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                        </div>
-                        <small class="text-muted">9 mins</small>
+                        <small class="text-muted">1500 руб.</small>
                     </div>
                 </div>
             </div>
@@ -125,48 +84,60 @@
 
         <div class="col">
             <div class="card shadow-sm">
-                <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+                <svg class="bd-placeholder-img card-img-top"   width="477" height="225"  xmlns="http://www.w3.org/2000/svg"  role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false">
+                    <image href="https://newflora.ru/wp-content/uploads/2019/05/kernew-Newflora.ru-1.jpg" class="img-fluid"   width="477" height="225"/> -->
+                    <!--<rect width="100%" height="100%" fill="#eceeef"></rect>-->
+
+                </svg>
 
                 <div class="card-body">
-                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <p class="card-text"> Букет из 3 видов роз. </p>
                     <div class="d-flex justify-content-between align-items-center">
                         <div class="btn-group">
-                            <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                            <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                            <button type="button" class="btn btn-sm btn-outline-secondary">Подробно</button>
+                            <button type="button" class="btn btn-sm btn-outline-secondary" style="background-color: violet">В корзину</button>
                         </div>
-                        <small class="text-muted">9 mins</small>
+                        <small class="text-muted">1500 руб.</small>
                     </div>
                 </div>
             </div>
         </div>
         <div class="col">
             <div class="card shadow-sm">
-                <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+                <svg class="bd-placeholder-img card-img-top"   width="477" height="225"  xmlns="http://www.w3.org/2000/svg"  role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false">
+                    <image href="https://newflora.ru/wp-content/uploads/2019/05/kernew-Newflora.ru-1.jpg" class="img-fluid"   width="477" height="225"/> -->
+                    <!--<rect width="100%" height="100%" fill="#eceeef"></rect>-->
+
+                </svg>
 
                 <div class="card-body">
-                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <p class="card-text"> Букет из 3 видов роз. </p>
                     <div class="d-flex justify-content-between align-items-center">
                         <div class="btn-group">
-                            <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                            <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                            <button type="button" class="btn btn-sm btn-outline-secondary">Подробно</button>
+                            <button type="button" class="btn btn-sm btn-outline-secondary" style="background-color: violet">В корзину</button>
                         </div>
-                        <small class="text-muted">9 mins</small>
+                        <small class="text-muted">1500 руб.</small>
                     </div>
                 </div>
             </div>
         </div>
         <div class="col">
             <div class="card shadow-sm">
-                <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+                <svg class="bd-placeholder-img card-img-top"   width="477" height="225"  xmlns="http://www.w3.org/2000/svg"  role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false">
+                    <image href="https://newflora.ru/wp-content/uploads/2019/05/kernew-Newflora.ru-1.jpg" class="img-fluid"   width="477" height="225"/> -->
+                    <!--<rect width="100%" height="100%" fill="#eceeef"></rect>-->
+
+                </svg>
 
                 <div class="card-body">
-                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <p class="card-text"> Букет из 3 видов роз. </p>
                     <div class="d-flex justify-content-between align-items-center">
                         <div class="btn-group">
-                            <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                            <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                            <button type="button" class="btn btn-sm btn-outline-secondary">Подробно</button>
+                            <button type="button" class="btn btn-sm btn-outline-secondary" style="background-color: violet">В корзину</button>
                         </div>
-                        <small class="text-muted">9 mins</small>
+                        <small class="text-muted">1500 руб.</small>
                     </div>
                 </div>
             </div>
