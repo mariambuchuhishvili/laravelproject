8f7c2b1fa3afa0a586c1580fb3b5dadddfa035ff
mariam <mariammuza@mail.ru>
2022-05-25 12:58:29 +0500

    commit_wednesday

--- resources/views/layouts/shablon.blade.php
+++ resources/views/layouts/shablon.blade.php
@@ -5,11 +5,14 @@
         <!--<meta name="viewport" content="width=device-width, initial-scale=1.0">-->
         <!--<meta http-equiv="X-UA-Compatible" content="ie=edge">-->
         <title>My page</title>
-        <link rel="stylesheet" href="css/app.css">
+        <!--<link rel="stylesheet" href="css/app.css">-->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
     </head>
-    <body>
-    @yield('header')
+    <body style="background-color: #9e7bc1">
+    <header> @yield('header')</header>
+
     @yield('content')
-    @yield('footer')
+
+    <footer> @yield('footer')</footer>
     </body>
     </html>
